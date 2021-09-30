9a15c6c22139a62728ee4a1218f79671252b3266
Ruth Navarrete <rnava021@ucr.edu>
2021-09-29 17:47:30 -0700

    Update index.js
    variable fix

--- jeffrey/index.js
+++ jeffrey/index.js
@@ -13,8 +13,8 @@ const FFXIV_SAT = '<@&' + FFXIV_role + '> -- dont forget about cactpot drawing a
 const fashion_report = '!frinfo'
 const FR_NEST = '<@&' + FR_role + '> -- dont forget to incubate nests'
 const FR_PUSH = '<@&' + FR_role + '> -- its a push week, are you able to attend?'
-const ROOMS_W = '<@&' + FR_role + '> -- bababooey its time to steal rooms in rivera for **wednesday**\nreserve **room 320** in rivera\nruth please take the 2 hour slot 4 - 6\nfahed please take the 2 hour slot 6-8'
-const ROOMS_A = '<@&' + FR_role + '> -- bababooey its time to steal rooms in rivera for **the week**'
+const ROOMS_W = '<@&' + FR_role + '> -- bababooey its time to steal rooms in rivera for wednesday\nreserve room 320 in rivera\nruth please take the 2 hour slot 4 - 6\nfahed please take the 2 hour slot 6-8'
+const ROOMS_A = '<@&' + FR_role + '> -- bababooey its time to steal rooms in rivera for the week'
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
@@ -37,23 +37,23 @@ client.on('messageCreate', msg => {
 // reminder ping functions
 // cactpot
 let cactpot = new cron.CronJob('45 18 * * Sat', () => {
-	client.channels.cache.get('888345534513958923').send(FFXIV_SAT);
-	client.channels.cache.get('888345534513958923').send(fashion_report).then(msg => msg.delete());
+	client.channels.cache.get(remind_channel).send(FFXIV_SAT);
+	client.channels.cache.get(remind_channel).send(fashion_report).then(msg => msg.delete());
 })
 
 // nesting
 let nesting = new cron.CronJob('00 9 * * *', () => {
-	client.channels.cache.get('888345534513958923').send(FR_NEST);
+	client.channels.cache.get(remind_channel).send(FR_NEST);
 })
 
 // rooms - '01 0 * * Thu'
-let res_w = new cron.CronJob('45 17 * * *', () => {
-	client.channels.cache.get('888345534513958923').send(ROOMS_W);
+let res_w = new cron.CronJob('50 17 * * *', () => {
+	client.channels.cache.get(remind_channel).send(ROOMS_W);
 })
 
 // rooms - '01 0 * * Sun'
-let res_a = new cron.CronJob('45 17 * * *', () => {
-	client.channels.cache.get('888345534513958923').send(ROOMS_A);
+let res_a = new cron.CronJob('50 17 * * *', () => {
+	client.channels.cache.get(remind_channel).send(ROOMS_A);
 })
 
 // start reminder ping functions
