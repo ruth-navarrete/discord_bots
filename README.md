e483fa3302de4d9204edfdf0dfd1969434994b52
Ruth Navarrete <rnava021@ucr.edu>
2022-03-24 18:28:19 -0700

    jeffery

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # discord_bots
 personal bots
+
+## jefferey: a reminder bot for me
+* pings weekly to remind for daily cactpot
--- jeffrey/index.js
+++ jeffrey/index.js
@@ -13,10 +13,6 @@ const remind_channel  = '888345534513958923';
 // messages
 const FFXIV_SAT       = '<@&' + FFXIV_role + '> -- dont forget about cactpot drawing at 6PM PST/7PM PDT and fasion report'
 const fashion_report  = '!frinfo'
-const FR_NEST         = '<@&' + FR_role + '> -- dont forget to incubate nests'
-const FR_PUSH         = '<@&' + FR_role + '> -- its a push week, are you able to attend?'
-const ROOMS_W         = '<@&' + FR_role + '> -- bababooey\nits time to steal rooms in rivera for **wednesday**\nreserve room **320** in rivera\n'
-const ROOMS_A         = '<@&' + FR_role + '> -- bababooey\nits time to steal rooms in rivera for the **week** in rivera 320\n\nruth please make sure you have: S 2-5 | M 4-7 | T 12-2 | R 12-2\n\nfahed please make sure you have W 5-8'
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
@@ -43,26 +39,8 @@ let cactpot = new cron.CronJob('45 18 * * Sat', () => {
 	client.channels.cache.get(remind_channel).send(fashion_report).then(msg => msg.delete());
 })
 
-// nesting
-let nesting = new cron.CronJob('00 9 * * *', () => {
-	client.channels.cache.get(remind_channel).send(FR_NEST);
-})
-
-// rooms wednesday reminder
-let res_w = new cron.CronJob('01 0 * * Thu', () => {
-	client.channels.cache.get(remind_channel).send(ROOMS_W);
-})
-
-// rooms weekly reminder
-let res_a = new cron.CronJob('01 0 * * Sat', () => {
-	client.channels.cache.get(remind_channel).send(ROOMS_A);
-})
-
 // start reminder ping functions
 cactpot.start();
-nesting.start();
-res_w.start();
-res_a.start();
 
 // Login to Discord with your client's token
 client.login(token);
